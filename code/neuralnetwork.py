0f64736affd5e9f4e6327f136e42179231979b0d
brennankneeland-beep <brennan.kneeland@brentwood.ca>
2026-05-30 16:18:08 -0700

    multiple neurons

--- code/neuralnetwork.py
+++ code/neuralnetwork.py
@@ -8,17 +8,31 @@ output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + in
 print(output)
 '''
 #layer of neurons
+
 inputs = [1, 2, 3, 2.5]
 weight1  = [0.2, 0.8, -0.5, 1.0]
 weight2  = [0.5, -0.91, 0.26, -0.5]
 weight3  = [-0.26, -0.27, 0.17, 0.87]
+weights = [weight1, weight2, weight3]
 bias1 = 2
 bias2 = 3
 bias3 = 0.5
-output = [
-    float(np.dot(inputs, weight1) + bias1),
-    float(np.dot(inputs, weight2) + bias2),
-    float(np.dot(inputs, weight3) + bias3)
-]
-
+biases = [bias1, bias2, bias3]
+output = np.dot(weight1, inputs) + bias1
 print(output)
+#this is the same as *
+outputs = np.dot(weights, inputs) + biases 
+print(outputs)
+#*
+output = [
+    float(np.dot(inputs, weights[0]) + bias1),
+    float(np.dot(inputs, weights[1]) + bias2),
+    float(np.dot(inputs, weights[2]) + bias3)
+]
+#*
+layer_outputs = []
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_output = float(np.dot(inputs, neuron_weights) + neuron_bias)
+    layer_outputs.append(neuron_output)
+print(output)
+print(layer_outputs)
