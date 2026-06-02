1a327b76b676f2e4b48a6d390358863b0889dc3e
brennankneeland-beep <brennan.kneeland@brentwood.ca>
2026-06-02 14:55:43 -0700

    ReLu activation function created

--- code/neuralnetwork.py
+++ code/neuralnetwork.py
@@ -4,6 +4,8 @@
 #for the next day to be displayed on a LCD screen.
 
 import numpy as np
+
+
 #single neuron with 4 inputs, weights and a bias
 '''
 inputs = [1, 2, 3, 2.5]
@@ -72,20 +74,53 @@ layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases2
 print(layer2_output)
 '''
 #make it an object
+import nnfs
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
+nnfs.init()
+X, y = spiral_data(100, 3)
+'''
 X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
+'''
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10* np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+
+'''
 layer1 = Layer_Dense(4, 5)
 layer2 = Layer_Dense(5, 2)
 layer1.forward(X)
 print(layer1.output)
 layer2.forward(layer1.output)
 print(layer2.output)
+'''
+#rectufied linear activation function
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output =[]
+for i in inputs:
+    if i > 0:
+        output.append(i)
+    elif i <= 0:
+        output.append(0)
+#same thing but with max function
+'''
+output = []
+for i in inputs:
+    output.append(max(0, i))
+print(output)
+'''
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
+layer1.forward(X)
+activation1.forward(layer1.output)
+#print(layer1.output)
+print(activation1.output)
