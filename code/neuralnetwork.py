f067beeb69e7235a2a6d0cf23a60a542ca3273b7
brennankneeland-beep <brennan.kneeland@brentwood.ca>
2026-06-05 16:44:17 -0700

    added backpropogation for layer dence with relu

--- code/neuralnetwork.py
+++ code/neuralnetwork.py
@@ -189,6 +189,7 @@ print(loss)
 loss = -math.log(softmax_output_example[target_class])
 print(loss)
 '''
+'''
 softmax_output_examples = np.array([[0.7, 0.1, 0.2],
                                     [0.5, 0.1, 0.4],
                                     [0.02, 0.9, 0.08]])
@@ -198,6 +199,7 @@ print(softmax_output_examples[range(len(softmax_output_examples)), class_Targets
 loss = -np.log(softmax_output_examples[range(len(softmax_output_examples)), class_Targets])
 print(loss)
 average_loss = np.mean(loss)
+'''
 class loss:
     def calculate(self, output, y):
         sample_losses = self.forward(output, y)
@@ -213,6 +215,141 @@ class Loss_CategoricalCrossentropy(loss):
             correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
+'''
 predictions = np.argmax(softmax_output_examples, axis=1)
 accuracy = np.mean(predictions == class_Targets)
 print('Accuracy:', accuracy)
+'''
+#back propogation for a singe neuron (minimize output)
+'''
+x = [1, -2, 3]#inputs
+w = [-3, -1, 2]#weights
+b = 1#bias
+for i in range(600):
+    xw0 = x[0]*w[0]
+    xw1 = x[1]*w[1]
+    xw2 = x[2]*w[2]
+    z = xw0 + xw1 + xw2 + b
+    #print(xw0, xw1, xw2)
+    #print(z)
+    y = max(0, z)
+    #print(y)
+    dval = 1
+    drelu_dz = dval * (1. if z > 0 else 0.)
+    #print(drelu_dz)
+    dsum_dxw0 = 1
+    drelu_dxw0 = drelu_dz * dsum_dxw0
+
+    dsum_dxw1 = 1
+    drelu_dxw1 = drelu_dz * dsum_dxw1
+
+    dsum_dxw2 = 1 
+    drelu_dxw2 = drelu_dz * dsum_dxw2
+
+    dsum_db = 1
+    drelu_db = drelu_dz * dsum_db
+    #print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
+    dmul_dx0 = w[0]
+    drelu_dx0 = drelu_dxw0 * dmul_dx0
+    dmul_dx1 = w[1]
+    drelu_dx1 = drelu_dxw1 * dmul_dx1
+    dmul_dx2 = w[2]
+    drelu_dx2 = drelu_dxw2 * dmul_dx2
+
+    dmul_dw0 = x[0]
+    drelu_dw0 = drelu_dxw0 * dmul_dw0
+    dmul_dw1 = x[1]
+    drelu_dw1 = drelu_dxw1 * dmul_dw1
+    dmul_dw2 = x[2]
+    drelu_dw2 = drelu_dxw2 * dmul_dw2
+    #print(drelu_dx0, drelu_dx1, drelu_dx2)
+    #print(drelu_dw0, drelu_dw1, drelu_dw2)
+    drelu_dx0 = dval*(1 if z > 0 else 0.)*w[0]
+    dx = [drelu_dx0, drelu_dx1, drelu_dx2]
+    dw = [drelu_dw0, drelu_dw1, drelu_dw2]
+    db = drelu_db
+    #print(w, b)
+    w[0] -= 0.001*dw[0]
+    w[1] -= 0.001*dw[1]
+    w[2] -= 0.001*dw[2]
+    b -= 0.001*db
+    print(y)
+print (w, b)
+'''
+#back propogation for a layer of neurons
+'''
+dvals = np.array([[1, 1, 1],
+                  [2, 2, 2],
+                  [3, 3, 3]])
+inputs = np.array([[1, 2, 3, 2.5],
+                   [2.0, 5.0, -1.0, 2.0],
+                   [-1.5, 2.7, 3.3, -0.8]])
+weights = np.array([[0.2, 0.8, -0.5, 1.0],
+                    [0.5, -0.91, 0.26, -0.5],
+                    [-0.26, -0.27, 0.17, 0.87]]).T
+biases = np.array([[2, 3, 0.5]])
+dbiases = np.sum(dvals, axis=0, keepdims=True)
+dweights = np.dot(inputs.T, dvals)
+print(dbiases)
+print(dweights)
+'''
+'''
+dx0 = sum(weights[0]*dvals[0])
+dx1 = sum(weights[1]*dvals[0])
+dx2 = sum(weights[2]*dvals[0])
+dx3 = sum(weights[3]*dvals[0])
+
+dinputs = np.array([dx0, dx1, dx2, dx3])
+print(dinputs)
+'''
+#same as above but with numpy
+'''
+dinputs = np.dot(dvals, weights.T)
+print(dinputs)
+z = np.array([[1, 2, -3, -4],
+              [2, -7, -1, 3],
+              [-1, 2, 5, -1]])
+dvals = np.array([[1, 2, 3, 4],
+                  [5, 6, 7, 8],
+                  [9, 10, 11, 12]])
+'''
+'''
+drelu = np.zeros_like(z)
+drelu[z > 0] = 1
+print(drelu)
+drelu *= dvals
+print(drelu)
+'''
+'''
+drelu = dvals.copy()
+drelu[z <= 0] = 0
+print(drelu)
+'''
+#finally: 
+dvals = np.array([[1, 1, 1],
+                  [2, 2, 2],
+                  [3, 3, 3]])
+inputs = np.array([[1, 2, 3, 2.5],
+                   [2.0, 5.0, -1.0, 2.0],
+                   [-1.5, 2.7, 3.3, -0.8]])
+weights = np.array([[0.2, 0.8, -0.5, 1.0],
+                    [0.5, -0.91, 0.26, -0.5],
+                    [-0.26, -0.27, 0.17, 0.87]]).T
+biases = np.array([[2, 3, 0.5]])
+
+layer_outputs = np.dot(inputs, weights) + biases
+relu_outputs = np.maximum(0, layer_outputs)
+
+drelu = dvals.copy()
+drelu[layer_outputs <= 0] = 0
+print(drelu)
+dinputs = np.dot(drelu, weights.T)
+dweights = np.dot(inputs.T, drelu)
+dbiases = np.sum(drelu, axis=0, keepdims=True)
+print(dinputs)
+print(dweights)
+print(dbiases)
+weights -= 0.001*dweights
+biases -= 0.001*dbiases
+print(weights)
+print(biases)
--- code/neuralnetworkBIG.py
+++ code/neuralnetworkBIG.py
@@ -11,12 +11,23 @@ class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10* np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
 class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
+        self.inputs = inputs
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
+
 
 class Activation_Softmax:
     def forward(self, inputs):
@@ -38,6 +49,8 @@ class Loss_CategoricalCrossentropy(loss):
             correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
+    
+
 X, y = spiral_data(samples = 100,classes = 3)
 
 dense1 = Layer_Dense(2, 3)
@@ -53,4 +66,7 @@ activation2.forward(dense2.output)
 print(activation2.output[:5])
 loss_function = Loss_CategoricalCrossentropy()
 loss = loss_function.calculate(activation2.output, y)
+predictions = np.argmax(activation2.output, axis=1)
+accuracy = np.mean(predictions == y)
 print('loss:', loss)
+print('accuracy:', accuracy)
